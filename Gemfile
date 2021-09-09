a23785181bda5e84625887169da4de2ed93de704
Tore Eschliman <tore.eschliman@instacart.com>
2021-09-09 12:45:34 -0500

    bring this in with fixes

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in cloudflare-rails.gemspec
+gemspec
+
+group :development do
+  gem "rspec-isolation", git: "https://github.com/modosc/rspec-isolation"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,37 @@
+# copied from https://codingdaily.wordpress.com/2011/01/14/test-a-gem-with-the-rails-3-stack/
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+ENV["RAILS_ENV"] ||= 'test'
+
+require 'bundler/setup'
+Bundler.setup
+
+require 'rubygems'
+require 'pry'
+
+# Only the parts of rails we want to use
+require "action_controller/railtie"
+require 'action_view/railtie'
+require "rails/test_unit/railtie"
+
+# pull in rspec/rails before cloudflare/rails since that'll pull in rails which
+# matches the ordering in a rails app
+require 'rspec/rails'
+require 'webmock/rspec'
+require 'rspec/isolation'
+
+if ENV['RACK_ATTACK'] == 'first'
+  # pull in rack/attack first to make sure patches work with it
+  require 'rack/attack'
+end
+
+require 'cloudflare/rails'
+
+if ENV['RACK_ATTACK'] == 'last'
+  # pull in rack/attack last to make sure patches work with it
+  require 'rack/attack'
+end
+
+RSpec.configure do |config|
+  config.infer_base_class_for_anonymous_controllers = false
+end
